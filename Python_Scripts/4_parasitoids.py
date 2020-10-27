53e06aa2f457feb6ca9440e7c68bfbd17ce77b86
KaRMaX-aka-flo <elkarmaxgaming@gmail.com>
2020-10-27 18:21:00 +0100

    Update 4_parasitoids.py

--- Python_Scripts/4_parasitoids.py
+++ Python_Scripts/4_parasitoids.py
@@ -5,29 +5,31 @@ import random
 
 def over_patches():
     Best_clutch = [0] * N_PATCH
+    Index = 1 + (X - X_CRITICAL)/X_INC
     Clutch = range(1, int(Index))
-    W = P_SURVIVAL * (1 - sum(P_BENEFIT)) * F_vectors[Index,1]
-    for i in range(Npatch):
-        W_partial = Benefit[range(1,int(Index)), i] + P_SURVIVAL * F.vectors[Index-Clutch-1,1]
+    W = P_SURVIVAL * (1 - sum(p_benefit)) * F_vectors[int(Index),1]
+    for i in range(N_PATCH):
+        W_partial = Benefit[range(1,int(Index)), i] + P_SURVIVAL * F_vectors[Index-Clutch-1,1]
         Best = W_partial.sort(reverse = TRUE)
         Best_clutch[i] = Best[0]
         W += p_benefit[i] * max(W.partial)
-        if((len(W.partial>1)) and (W.partial[Best[0]] == W.partial[Best[1]])):
+        if((len(W_partial>1)) and (W_partial[Best[0]] == W_partial[Best[1]])):
             print("Several possible equal choices")
-    F_vectors[Index - 1, 0] = W
-    Temp = (F_vectors[Index - 1, 0], Best_Clutch[HOST_TYPE])
+    F_vectors[int(Index) - 1, 0] = W
+    Temp = (F_vectors[int(Index) - 1, 0], Best_Clutch[HOST_TYPE])
     F_vectors.append(Temp)
     return(F_vectors)
 
 def over_states():
     for i in range(1, int(MAX_INDEX)):
-        X = (Index - 1) * X_INC + X_CRITICAL
+        X = (i - 1) * X_INC + X_CRITICAL
         Temp = over_patches()
         D = Temp[-1,:]
         del(Temp[-1,:])
         F_vectors = np.transpose(Temp)
         np.c_[F_vectors, D]
     return(F_vectors)
+
 
 X_MAX = 40
 X_CRITICAL = 0
@@ -37,7 +39,6 @@ p_benefit = [0.05, 0.05, 0.1, 0.8]
 P_SURVIVAL = 0.99
 MAX_INDEX = 1 + (X_MAX - X_CRITICAL)/X_INC
 ##INDEX = range(1,int(MAX_INDEX) + 1)
-Index = 1 + (X - X_CRITICAL)/X_INC
 F_vectors = np.zeros((int(MAX_INDEX), 2))
 Clutch = range(X_MAX + 1)
 Benefit = np.zeros((X_MAX + 1, 4))
@@ -53,9 +54,10 @@ host_coef[1,:] = [-0.1444, 2.2997, -0.1170, 0.0013]
 host_coef[2,:] = [-0.1048, 2.2097, -0.0878, 0.0004222]
 host_coef[3,:] = [-0.0524, 2.0394, -0.0339, -0.0003111]
 
-def main():
-    for i in range(4):
-        Benefit[:, i] = host_coef[i, 0] + hist_coef[i, 1] * Clutch + host_coef[i, 2] * Clutch**2 + host_coef[i, 3] * Clutch**3
+def main(HostType, Output1, Output2):
+    for i in range(41):
+        for j in range(4):
+            Benefit[i, j] = host_coef[j, 0] + host_coef[j, 1] * Clutch[i] + host_coef[j, 2] * Clutch[i]**2 + host_coef[j, 3] * Clutch[i]**3
     Benefit[0,:] = 0
     Time = HORIZON
     while Time > 1:
@@ -72,15 +74,22 @@ def main():
     with np.printoptions(precision = 3):
         print(FxtT[:, range(int(MAX_INDEX) + 1)])
     
-        #plot en 3D, google est ton ami
     y = Best_patch[HORIZON, range(1, int(MAX_INDEX) + 1)]
     x = range(HORIZON)
+    #plot en 3D, google est ton ami
+    ovip = cbind(x, Best.Patch[range(int(HORIZON) -1),40])
+    ovip = t(ovip)
+    np.savetxt(Output1, ovip, fmt="%.0f")
+# Enregistrement des matrices de décision pour chaque type d'hôte
+    bpatch = Best.Patch[range(int(HORIZON) -1),range(1,41)]
+    bpatch = t(bpatch)
+    np.savetxt(Output2, bpatch, fmt="%.0f")
     return()
     
 HostType = 1
 Output1 = "OVOPOSITION1.txt"
 Output2 = "DM1.txt"
-main()
+main(HostType, Output1, Output2)
 
 ## Idem pour l'hôte n°2
 HostType = 2
@@ -99,3 +108,71 @@ HostType = 4
 Output1 = "OVOPOSITION4.txt"
 Output2 = "DM4.txt"
 main()
+
+
+########################################
+#### Partie prédiction individuelle ####
+########################################
+
+rm(list = ls()) # Remove all objects from memory
+Xmax <- 40
+# Maximum value of X = eggs
+DM1 = read.table(file = "DM1.txt") # Cols = x rows = time
+DM2 <- read.table(file = "DM2.txt")
+DM3 <- read.table(file = "DM3.txt")
+DM4 <- read.table(file = "DM4.txt")
+# Create an array for Decision matrix
+
+DM <- array(0,c(20,Xmax,4)) # time, state, host
+for (i in 1:20) {
+  for (j in 1:Xmax) {
+    DM[i,j,1] <- DM1[i,j]
+    DM[i,j,2] <- DM2[i,j]
+    DM[i,j,3] <- DM3[i,j]
+    DM[i,j,4] <- DM4[i,j]
+  }
+}
+
+# Probability of encountering host type
+Pbenefit <- c(0.05, 0.05, 0.1, 0.8)
+Times <- c(5,5,10,80) 
+Host.Type <- c(rep(1,Times[1]),rep(2,Times[2]),rep(3,Times[3]), rep(4,Times[4])) # Create Vector for Host type probability
+Psurvival <- 0.99 # Survival probability per time increment
+Horizon <- 10 # Number of time steps
+set.seed(10) # Initialise random number generator
+N.Ind <- 1000 # Number of individuals
+Output <- matrix(0,N.Ind,Horizon) # Allocate space for output
+# Generate initial values of x from normal distribution
+x.init <- ceiling(rnorm(N.Ind, mean = 20, sd = 5))
+
+for (Ind in 1:N.Ind) { # Iterate over individuals
+  # Generate vectors for choosing the Host type and probability of survival
+  Host <- ceiling(100*runif(Horizon)) # Vector of host types
+  Survival <- runif(Horizon) # Vector of survival probabilities
+  Survival[Survival>Psurvival] <- 0 # Set all values of Survival > Psurvival = 0
+  Survival[Survival!=0] <- 1 # Set all other values to 1
+  x <- x.init[Ind] # Initial value of x
+  
+  for (Time in 1:Horizon){ # Iterate over time periods
+    if(x>0) { # If eggs remaining calculate clutch size using DM
+      Clutch.Size <- DM[Time,x,Host.Type[Host[Time]]]
+      Output[Ind,Time] <- Clutch.Size # Store clutch size
+      # Compute new value of x
+      x <- x - Clutch.Size
+    }
+    x <- x*Survival[Time] # Set x = 0 if female does not survive
+  } # end of Time loop
+} # End of Ind loop
+
+par(mfcol = c(5,2)) # Set graphics page to 5 rows and 2 columns
+# Iterate over time and plot bar graphs of clutch size
+for (i in 1:10 ){
+  Data <-Output[,i]
+  Data <- Data[Data>0] # Eliminate zeroes
+  xbar <- mean(Data) # Mean clutch size
+  
+  print(c(i, mean(Data))) # Time, Output mean clutch size
+  Data <- table(Data) # Tabulate data
+  # Plot data using a bar graph, because x is integral
+  barplot(Data, xlab = "Clutch Size", space = 0, xlim = c(0,5), main = paste("Time =",i), col = 1)
+}
