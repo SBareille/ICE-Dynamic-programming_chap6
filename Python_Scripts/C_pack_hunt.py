4e51617d8619cf7d18fa3faa6824f6a6e45c5b2a
SBareille <servanebareille@hotmail.fr>
2020-10-28 19:22:14 +0100

    quasi fini
    problème colonne 1 en sortie (et time 31)

--- Python_Scripts/C_pack_hunt.py
+++ Python_Scripts/C_pack_hunt.py
@@ -11,7 +11,7 @@ import scipy.stats
 def fitness(x_state, benefit_row_i, p_benefit_row_i, f_vectors):
     """ Computes the fitness of a hunter in specific pack size, knowing its state is x_state.
 
-    Keyword arguments:
+    Arguments:
     x_state -- state of the hunter
     benefit_row_i -- State benefit for one hunter if it mades 0 to 4 kills with its pack of size i.
     p_benefit_row_i --  # Probability of killing 0 to 4 preys by a pack of size i
@@ -24,70 +24,55 @@ def fitness(x_state, benefit_row_i, p_benefit_row_i, f_vectors):
         x_state = x_store - COST + benefit_row_i[i_kill] # Calculate new state value
         x_state = min(x_state, X_MAX)
         x_state = max(x_state, X_CRITICAL)
-        index = 1 + int((x_state - X_CRITICAL)/X_INC) # (arrondi en dessous)
-        index = min(index, INDEX_MAX)
+        index = int((x_state - X_CRITICAL)/X_INC) # (arrondi en dessous)
+        index = min(index, INDEX_MAX-1)
 
         qx = x_state - int(x_state) # qx for linear interpolation
         term1 = qx * f_vectors[index, 1]
-        term2 = (1 - qx) * f_vectors[index-1, 1]
+        term2 = (1 - qx) * f_vectors[index, 1]
         w_patch = w_patch + p_benefit_row_i[i_kill] * (term1 + term2)
     return w_patch
 
-def over_patches(x_state):
+def over_patches(x_state, f_vectors):
     """ scenario 3
     """
-    rhs = np.zeros([1, N_PATCH])  # Saves the fitnesses for the 3 patches
+    """rhs = np.zeros([1, N_PATCH])  # Saves the fitnesses for the 3 patches
     for i in range(N_PATCH):
-        rhs[i] = fitness()
+        rhs[0,i] = fitness(x_state, benefit[i,], p_benefit[i,], f_vectors)
     max(rhs)
-    index = 1 + int((x - X_CRITICAL)/X_INC)
-    # index = min(index, INDEX_MAX)
-
-    f_vectors1 = np.matrix.copy(f_vectors)
-    f_vectors1[index - 1,0] = max(rhs)
+    index = int((x_state - X_CRITICAL)/X_INC)
+    index = min(index, INDEX_MAX)
+    f_vectors[index, 0] = np.amax(rhs)
+    #ok jusque là"""
+    
+    rhs = [0] * N_PATCH  # Saves the fitnesses for the 3 patches
+    for i in range(N_PATCH):
+        rhs[i] = fitness(x_state, benefit[i,], p_benefit[i,], f_vectors)
+    index = int((x_state - X_CRITICAL)/X_INC)
+    index = min(index, INDEX_MAX)
+    f_vectors[index, 0] = max(rhs)
     best_patch = rhs.index(max(rhs)) + 1
-    mat_temp = np.append(f_vectors1,[[f_vectors1[index - 1,0],best_patch]],axis=0)
 
-    # il semble que pour opti on puisse faire : 
-    # f_vectors[index - 1, 0] = max(rhs)
-    # best_patch = rhs.index(max(rhs)) + 1
-    
-    if len(np.where(max(rhs)) > 1 :
-        choices[time,] = [1,1] # vérifier si c'est time ou time-1 ou +1
-
-    # return [f_vectors[index - 1, 0], best_patch] # si opti
-    return mat_temp
+    rhs=np.asarray(rhs,dtype=np.float32)
+    choice = 0
+    if np.shape(np.where(rhs == max(rhs)))[1] > 1 :
+        choice = 1 # vérifier si c'est time ou time-1 ou +1
+    return [f_vectors[index - 1, 0], best_patch, choice]
 
 
-def over_states():
+def over_states(time, mat_best_patch, fxtt, choices, f_vectors):
     for index in list(range(2, INDEX_MAX + 1)):
-        x = (index - 1) * X_INC + X_CRITICAL
-        temp = over_patches()
-        #fxtt[time, x_state - 1] = temp[0]
-        #mat_best_patch[time, x_state - 1] = temp[1]
-    
-    return mat_temp
-  
-
-    n <- nrow(temp)- 2
-    f_vectors = temp- Temp[1:n,]
-    Store[Index,1:2] <- Temp[n+1,] # Save F(x,t,T) and best patch
-    Store[Index,3] <- Temp[n+2,1] # 
-
-    
-    n <- nrow(Temp)-1
-    F.vectors <- Temp[1:n,]
-    Store[X,] <- Temp[n+1,] # Save F(x,t) and best patch
-    } # End of X loop
-  
-  # Add Store values to end of F.vectors for pass back to main program
-  Temp <- cbind(F.vectors, Store) # Combined by columns       
+        x_state = (index - 1) * X_INC + X_CRITICAL # va bien de 1 à X_MAX
+        temp = over_patches(x_state, f_vectors)
+        fxtt[time, x_state - 1] = temp[0]
+        mat_best_patch[time, x_state - 1] = temp[1]
+        choices[time, x_state - 1] = temp[2]
 
 
 # Initialize parameters
 X_MAX = 30 # Maximum value of X = gut capacity
 X_CRITICAL = 0 # Value of X (gut content) at which death occurs
-X_INC = 0.33 # Increment in state variable
+X_INC = 1 # Increment in state variable
 INDEX_MAX = 1 + int((X_MAX - X_CRITICAL)/X_INC)# Maximum index value, floor (arondi en dessous)
 N_PATCH = 4  # Number of patches = packs
 COST = 6 # Daily food requirement
@@ -104,8 +89,40 @@ for pack_size in range(1, 5) : # Iterate over pack sizes
 
 HORIZON = 31  # Number of time steps
 
-f_vectors = np.zeros([INDEX_MAX, 2])
-f_vectors[1:INDEX_MAX, 1] = 1
-fxtt = np.zeros([HORIZON, INDEX_MAX])
-mat_best_patch = np.zeros([HORIZON, INDEX_MAX])
-choices = np.zeros([HORIZON, INDEX_MAX]) # Matrix for flag indicating multiple equivalent choices. 0 = only one choice, 1 = more than one choice
+
+def main():
+    """" ATTENTIONNNNN TROUVER NOM """
+    # Initialisation of the f_vector matrix that enables the calcultation of
+    # fitnesses at time t using the ones in time t+1.
+    # In the second column we have fitnesses in time t+1 (used for calculation)
+    # and in the first column the updated values of fitness in time t.
+    # The survival at time = HORIZON is 1 for all state > X_CRITICAL
+    # because we are only interested in individuals that are alive at the end
+    # of the time period.
+    f_vectors = np.zeros([INDEX_MAX, 2])
+    f_vectors[1:INDEX_MAX, 1] = 1
+
+    # Initialisation of the best_patch and fxtt matrixes which keep for each
+    # time and each state the best_patch to choose and the fitness associated.
+    fxtt = np.zeros([HORIZON, INDEX_MAX])
+    mat_best_patch = np.zeros([HORIZON, INDEX_MAX])
+     # Matrix for flag indicating multiple equivalent choices. 0 = only one choice, 1 = more than one choice
+    choices = np.zeros([HORIZON, INDEX_MAX])
+
+    # We go back in time from t=HORIZON, when individuals are alive, to t=1,
+    # as we calculate fitness in time t thanks to the fitness in time t+1
+    # For each time we calculate a lign of best_patches and fxtt,
+    # and then pass the values of fitness into the second column of f_vector.
+    for time in reversed(range(HORIZON-1)):
+        over_states(time, mat_best_patch, fxtt, choices, f_vectors)
+        f_vectors[:, 1] = np.matrix.copy(fxtt[time, :])
+
+    # Finally, we output the best_patch and fxtt matrixes after formating
+    mat_best_patch[HORIZON - 1, :] = list(range(1, INDEX_MAX+1))
+    fxtt[HORIZON - 1, : ] = list(range(1, INDEX_MAX+1))
+    choices[HORIZON - 1, : ] = list(range(1, INDEX_MAX+1))
+    return(mat_best_patch, fxtt, choices)
+
+if __name__ == "__main__":
+    with np.printoptions(precision=3):
+        print(main()[0],"\n", main()[1],"\n", main()[2])
