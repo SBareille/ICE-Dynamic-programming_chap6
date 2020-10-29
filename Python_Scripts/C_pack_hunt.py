407c2bc652612047053074375a16a3e2bdea0204
SBareille <servanebareille@hotmail.fr>
2020-10-29 16:43:44 +0100

    index fixed

--- Python_Scripts/C_pack_hunt.py
+++ Python_Scripts/C_pack_hunt.py
@@ -1,9 +1,20 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Tue Oct 27 21:19:05 2020
+""" 
 
-@author: serva
+Usage :
+======
+python C_pack hunting_interpolation.py
 """
+
+__authors__ = ("BAREILLE Servane", "BOURGADE Clément", "PECHIERAS Florian", \
+               "WOEHREL Aubin")
+__contact__ = (" servane.bareille@etudiant.univ-rennes1.fr ", \
+               " clement.bourgade@etudiant.univ-rennes1.fr ", \
+               " florian.pechieras@etudiant.univ-rennes1.fr ", \
+               " aubin.woerel@etudiant.univ-rennes1.fr ")
+__copyright__ = " copyleft "
+__date__ = "29/10/2020"
+
 import numpy as np
 import scipy.stats
 #from scipy.stats import binom
@@ -13,8 +24,8 @@ def fitness(x_state, benefit_row_i, p_benefit_row_i, f_vectors):
 
     Arguments:
     x_state -- state of the hunter
-    benefit_row_i -- State benefit for one hunter if it mades 0 to 4 kills with its pack of size i.
-    p_benefit_row_i --  # Probability of killing 0 to 4 preys by a pack of size i
+    benefit_row_i -- State benefit for one hunter if it mades 0 to 3 kills with its pack of size i.
+    p_benefit_row_i --  Probability of killing 0 to 3 preys by a pack of size i
     f_vectors -- enables the calcultation of fitnesses at time t
                 using the ones in time t+1
     """
@@ -36,6 +47,7 @@ def fitness(x_state, benefit_row_i, p_benefit_row_i, f_vectors):
 def over_patches(x_state, f_vectors):
     """ scenario 3
     """
+    
     """rhs = np.zeros([1, N_PATCH])  # Saves the fitnesses for the 3 patches
     for i in range(N_PATCH):
         rhs[0,i] = fitness(x_state, benefit[i,], p_benefit[i,], f_vectors)
@@ -49,7 +61,6 @@ def over_patches(x_state, f_vectors):
     for i in range(N_PATCH):
         rhs[i] = fitness(x_state, benefit[i,], p_benefit[i,], f_vectors)
     index = int((x_state - X_CRITICAL)/X_INC)
-    index = min(index, INDEX_MAX)
     f_vectors[index, 0] = max(rhs)
     best_patch = rhs.index(max(rhs)) + 1
 
@@ -57,31 +68,38 @@ def over_patches(x_state, f_vectors):
     choice = 0
     if np.shape(np.where(rhs == max(rhs)))[1] > 1 :
         choice = 1 # vérifier si c'est time ou time-1 ou +1
-    return [f_vectors[index - 1, 0], best_patch, choice]
+    return [f_vectors[index, 0], best_patch, choice]
 
 
 def over_states(time, mat_best_patch, fxtt, choices, f_vectors):
-    for index in list(range(2, INDEX_MAX + 1)):
+    for index in list(range(X_CRITICAL + 1, INDEX_MAX)):
+        x_state = index * X_INC + X_CRITICAL # va bien de 1 à X_MAX
+        temp = over_patches(x_state, f_vectors)
+        fxtt[time, index] = temp[0]
+        mat_best_patch[time, index] = temp[1]
+        choices[time, index] = temp[2]
+   
+    """for index in list(range(2, INDEX_MAX + 1)):
         x_state = (index - 1) * X_INC + X_CRITICAL # va bien de 1 à X_MAX
         temp = over_patches(x_state, f_vectors)
         fxtt[time, x_state - 1] = temp[0]
         mat_best_patch[time, x_state - 1] = temp[1]
-        choices[time, x_state - 1] = temp[2]
+        choices[time, x_state - 1] = temp[2]"""
 
 
 # Initialize parameters
 X_MAX = 30 # Maximum value of X = gut capacity
 X_CRITICAL = 0 # Value of X (gut content) at which death occurs
 X_INC = 1 # Increment in state variable
-INDEX_MAX = 1 + int((X_MAX - X_CRITICAL)/X_INC)# Maximum index value, floor (arondi en dessous)
+INDEX_MAX = int((X_MAX - X_CRITICAL)/X_INC)# Maximum index value, floor (arondi en dessous)
 N_PATCH = 4  # Number of patches = packs
 COST = 6 # Daily food requirement
 
 Y = 11.25 # Size of single prey
 k = np.array([0,1,2,3]) # Number of kills
 Pi = [0.15, 0.31, 0.33, 0.33]  # Probability of single kill for pack size
-benefit = np.zeros([N_PATCH, len(k)])  
-p_benefit = np.zeros([N_PATCH, len(k)])  # Probability of finding food in the 3 patches
+benefit = np.zeros([N_PATCH, len(k)])  # Rows = pack size + 1, Columns = number of kills
+p_benefit = np.zeros([N_PATCH, len(k)])  # Probability of finding food in the 3 patches # Rows = pack size +1 , Columns = number of kills
 
 for pack_size in range(1, 5) : # Iterate over pack sizes
     p_benefit[pack_size-1,:] = scipy.stats.binom.pmf(k, 3, Pi[pack_size-1]) # Calculate binomial probabilities using function dbinom
@@ -99,30 +117,32 @@ def main():
     # The survival at time = HORIZON is 1 for all state > X_CRITICAL
     # because we are only interested in individuals that are alive at the end
     # of the time period.
-    f_vectors = np.zeros([INDEX_MAX, 2])
+    f_vectors = np.zeros([INDEX_MAX + 1, 2])
     f_vectors[1:INDEX_MAX, 1] = 1
 
     # Initialisation of the best_patch and fxtt matrixes which keep for each
     # time and each state the best_patch to choose and the fitness associated.
-    fxtt = np.zeros([HORIZON, INDEX_MAX])
-    mat_best_patch = np.zeros([HORIZON, INDEX_MAX])
+    fxtt = np.zeros([HORIZON, INDEX_MAX + 1])
+    mat_best_patch = np.zeros([HORIZON, INDEX_MAX + 1])
      # Matrix for flag indicating multiple equivalent choices. 0 = only one choice, 1 = more than one choice
-    choices = np.zeros([HORIZON, INDEX_MAX])
+    choices = np.zeros([HORIZON, INDEX_MAX + 1])
 
     # We go back in time from t=HORIZON, when individuals are alive, to t=1,
     # as we calculate fitness in time t thanks to the fitness in time t+1
     # For each time we calculate a lign of best_patches and fxtt,
     # and then pass the values of fitness into the second column of f_vector.
-    for time in reversed(range(HORIZON-1)):
+    for time in reversed(range(HORIZON)):
         over_states(time, mat_best_patch, fxtt, choices, f_vectors)
         f_vectors[:, 1] = np.matrix.copy(fxtt[time, :])
+        # print(f_vectors)
 
     # Finally, we output the best_patch and fxtt matrixes after formating
-    mat_best_patch[HORIZON - 1, :] = list(range(1, INDEX_MAX+1))
-    fxtt[HORIZON - 1, : ] = list(range(1, INDEX_MAX+1))
-    choices[HORIZON - 1, : ] = list(range(1, INDEX_MAX+1))
+    mat_best_patch[HORIZON - 1, :] = list(range(0, INDEX_MAX + 1))
+    fxtt[HORIZON - 1, : ] = list(range(0, INDEX_MAX + 1))
+    choices[HORIZON - 1, : ] = list(range(0, INDEX_MAX + 1))
     return(mat_best_patch, fxtt, choices)
 
+
 if __name__ == "__main__":
-    with np.printoptions(precision=3):
-        print(main()[0],"\n", main()[1],"\n", main()[2])
+    np.set_printoptions(precision=3, suppress=True)
+    print(main()[0],"\n", main()[1],"\n", main()[2])
